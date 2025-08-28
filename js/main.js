08420929bc680cb8371bb287640c433e6fb63175
Pablixz <pablocabezas2018@gmail.com>
2025-08-27 21:29:36 -0400

    update de partiarls y pages

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // codigo para implemetar el footer y nav
 
-fetch("partials/nav.html")
+fetch("/partials/nav.html")
   .then(res => res.text())
   .then(data => document.getElementById("nav").innerHTML = data);
 
-fetch("partials/footer.html")
+fetch("/partials/footer.html")
   .then(res => res.text())
   .then(data => document.getElementById("footer").innerHTML = data);
