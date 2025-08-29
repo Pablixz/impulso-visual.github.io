d3fd9763db875d1d96c4e6111a6a2191347b7851
Pablixz <pablocabezas2018@gmail.com>
2025-08-28 23:02:04 -0400

    ver2.0 correcto funcionamiento de pagina

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// codigo para implemetar el footer y nav
-
-fetch("/partials/nav.html")
-  .then(res => res.text())
-  .then(data => document.getElementById("nav").innerHTML = data);
-
-fetch("/partials/footer.html")
-  .then(res => res.text())
-  .then(data => document.getElementById("footer").innerHTML = data);
